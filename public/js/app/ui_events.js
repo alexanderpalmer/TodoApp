937a91bb0280fbb7fb189ca9e23b1c59687c0023
Alexander Palmer <apalmer@zbw.ch>
2021-06-07 19:26:38 +0200

    modale Dialoge sowie badgets integriert

--- public/js/app/ui_events.js
+++ public/js/app/ui_events.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const offcanvasSettings = new bootstrap.Offcanvas(document.getElementById('offcanvasSettings'));
     const modalSignIn = new bootstrap.Modal(document.getElementById('modalSignIn'));
+    const modalLogIn = new bootstrap.Modal(document.getElementById('modalLogIn'));
+    const modalRequestPassword = new bootstrap.Modal(document.getElementById('modalRequestPassword'));
 
     const bindEventListener = () => {
         document.getElementById('settingsSymbol').addEventListener('click', mainNavigationShow);
         document.getElementById('signin').addEventListener('click', modalSignInShow);
+        document.getElementById('login').addEventListener('click', modalLogInShow);
+        document.getElementById('modalLogInRegister').addEventListener('click', modalLogInRegister);
+        document.getElementById('modalLogInPasswordRequest').addEventListener('click', modalPasswordRequest);
+
     }
 
     const mainNavigationShow = () => {
@@ -15,10 +21,33 @@ document.addEventListener('DOMContentLoaded', () => {
         offcanvasSettings.hide();
     }
 
+    const modalLogInShow = () => {
+        mainNavigationHide();
+        modalLogIn.show();
+    }
+
+    const modalLogInHide = () => {
+        modalLogIn.hide();
+    }
+
+    const modalLogInRegister = () => {
+        modalLogInHide();
+        modalSignInShow();
+    }
+
+    const modalPasswordRequest = () => {
+        modalLogInHide();
+        modalRequestPassword.show();
+    }
+
     const modalSignInShow = () => {
         mainNavigationHide();
         modalSignIn.show();
     }
+
+
+
+
 
     bindEventListener();
 })
